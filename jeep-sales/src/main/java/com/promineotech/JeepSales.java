368be67d4c687d4e94b4c0376a27e308553bbbe1
cuicattack <47648622+cuicattack@users.noreply.github.com>
2023-01-04 18:44:37 -0800

    fixed version

--- jeep-sales/src/main/java/com/promineotech/JeepSales.java
+++ jeep-sales/src/main/java/com/promineotech/JeepSales.java
@@ -1,14 +1,14 @@
-package com.promineotech;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.SpringApplication;
-
-@SpringBootApplication
-
-public class JeepSales {
-
-	public static void main(String[] args) {
-		// start spring boot
-		SpringApplication.run(JeepSales.class, args);
-	}
-
-}
+//package com.promineotech;
+//import org.springframework.boot.autoconfigure.SpringBootApplication;
+//import org.springframework.boot.SpringApplication;
+//
+//@SpringBootApplication
+//
+//public class JeepSales {
+//
+//	public static void main(String[] args) {
+//		// start spring boot
+//		SpringApplication.run(JeepSales.class, args);
+//	}
+//
+//}
---
+++ jeep-sales/src/main/java/com/promineotech/JeepSales/JeepSales.java
@@ -0,0 +1,14 @@
+package com.promineotech.JeepSales;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.SpringApplication;
+
+@SpringBootApplication
+
+public class JeepSales {
+
+	public static void main(String[] args) {
+		// start spring boot
+		SpringApplication.run(JeepSales.class, args);
+	}
+
+}
